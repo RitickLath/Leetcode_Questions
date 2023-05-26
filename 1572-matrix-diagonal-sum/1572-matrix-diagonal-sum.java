f3ab0a113b046f3936d427bc841e568463a530c9
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-05-27 00:56:34 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (7.58%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -3,15 +3,15 @@ class Solution {
         int sum = 0;
         int j = 0;
         int k = mat.length-1;
-        int n = mat.length;
+        
         for(int i = 0 ; i<mat.length ; i++){
             sum = sum + mat[i][j] + mat[i][k];
             j++;
             k--;
             
         }
-        if((n+1)%2==0){
-            sum = sum - mat[n/2][n/2];
+        if(( mat.length+1)%2==0){
+            sum = sum - mat[ mat.length/2][ mat.length/2];
         }
         return sum;
         
