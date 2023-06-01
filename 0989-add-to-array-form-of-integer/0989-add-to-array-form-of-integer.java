a9f002d00131533ebbe4b0741c92dcff1ab759ed
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-02 03:26:30 +0530

    Time: 8 ms (33.14%), Space: 44.6 MB (28.44%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        int n = num.length-1;
+        int sum;
+        for(int i = 0 ; i<num.length ; i++){
+            sum = num[n] + k;
+            if(n==0){num[n] = sum;break;}
+            else{num[n] = sum%10;}
+            
+            n--;
+            k = sum/10;
+        }
+        
+        for(int i = 0 ; i<num.length ; i++){
+            String s = "" + num[i];
+            for(int j = 0 ; j<s.length() ; j++){
+                arr.add(s.charAt(j)-'0');
+            }
+        }
+        return arr;
+    }
+}
