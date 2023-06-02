e6601160333223977076e572ca296b50e35e618c
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-03 02:50:54 +0530

    Time: 2 ms (80.73%), Space: 43.7 MB (25.30%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,26 +1,17 @@
-import java.util.ArrayList;
-
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
-            arr.add(nums[i]);
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            int num = arr.get(i);
-            arr.remove(i);
-            if (arr.contains(target - num)) {
-                int index = arr.indexOf(target - num);
-                if (index >= i) {
-                    index++; // Adjust index if it is greater than or equal to i
-                }
-                return new int[]{i, index};
+            int c = target - nums[i];
+            if (map.containsKey(c)) {
+                return new int[]{map.get(c), i};
             }
-            arr.add(i, num); // Add the element back at the same index
+            map.put(nums[i], i);
         }
 
         return new int[]{1, 1};
     }
 }
+
+
