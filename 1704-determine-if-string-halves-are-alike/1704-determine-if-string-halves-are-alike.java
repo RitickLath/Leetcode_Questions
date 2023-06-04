f03d94547515ccd33c7af7861d02b448d52693f2
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-05 04:25:06 +0530

    Time: 3 ms (90.57%), Space: 41.1 MB (52.46%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,24 +1,14 @@
 class Solution {
-    public static boolean isVowel(char c) {
-        //c = Character.toLowerCase(c);
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
-
+    protected static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+        || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
-    
     public boolean halvesAreAlike(String s) {
-        int c = 0;
-        
-        
-        for (int i = 0; i < s.length()/2; i++) {
-            if(isVowel(s.charAt(i))) {
-                    c++;
-            }
-            if(isVowel(s.charAt(s.length()-1-i))){
-                c--;
-            }
-            
+        int checkingHalves = 0;
+        for(int i = 0, j = s.length() - 1; i < s.length() / 2; i++,j--){
+            if(isVowel(s.charAt(i))) checkingHalves++;
+            if(isVowel(s.charAt(j))) checkingHalves--;
         }
-        
-        return c == 0;
+        return checkingHalves == 0;
     }
 }
