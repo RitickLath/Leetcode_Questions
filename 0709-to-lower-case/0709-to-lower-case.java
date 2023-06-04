534e5bfcb0dd4c2569042bd0e9830bc3743dc662
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-05 03:54:51 +0530

    Time: 1 ms (17.52%), Space: 40.9 MB (41.98%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,5 +1,16 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder str = new StringBuilder();
+
+        for(int i = 0 ; i<s.length() ; i++){
+            if((int)s.charAt(i)>=65 && (int)s.charAt(i)<=90){
+                str.append((char)('a'+(s.charAt(i)-'A')));
+            }
+            else{
+                str.append(s.charAt(i));
+            }
+            
+        }
+        return str.toString();
     }
 }
