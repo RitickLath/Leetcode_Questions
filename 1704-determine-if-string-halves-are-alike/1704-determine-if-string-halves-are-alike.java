0c7f5372443eb2bb5cc7afc891a73aefecd14f95
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-05 04:06:30 +0530

    Time: 7 ms (34.57%), Space: 41 MB (52.46%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -4,13 +4,13 @@ class Solution {
         int a = 0;
         int b = 0;
         for(int i = 0 ; i<s.length()/2 ; i++){
-            if(s.charAt(i)-'a'==0 || s.charAt(i)-'e'==0 || s.charAt(i)-'i'==0 || s.charAt(i)-'o'==0 || s.charAt(i)-'u'==0){
+            if(s.charAt(i) == 'a' || s.charAt(i)== 'e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
                 a++;
             }
         }
         
         for(int i = s.length()/2 ; i<s.length() ; i++){
-            if(s.charAt(i)-'a'==0 || s.charAt(i)-'e'==0 || s.charAt(i)-'i'==0 || s.charAt(i)-'o'==0 || s.charAt(i)-'u'==0){
+            if(s.charAt(i) =='a' || s.charAt(i) =='e' || s.charAt(i) =='i' || s.charAt(i) =='o' || s.charAt(i) =='u'){
                 b++;
             }
         }
