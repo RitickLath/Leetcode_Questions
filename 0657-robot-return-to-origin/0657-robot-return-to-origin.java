37e261b25f49459fc2f2ded011596f88723c7ec3
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-05 15:22:06 +0530

    Time: 10 ms (33.35%), Space: 43.7 MB (15.29%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -4,8 +4,6 @@ class Solution {
         int u =0;
         int r =0;
         
-        
-        
         for(int i = 0; i<moves.length() ; i++){
             if(moves.charAt(i) == 'R'){r++;}
             else if(moves.charAt(i) == 'D'){u--;}
