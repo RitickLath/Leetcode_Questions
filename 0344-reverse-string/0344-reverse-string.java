3532a604984ce62b8fba5b6121a8886362b9e667
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-05 15:33:01 +0530

    Time: 1 ms (92.93%), Space: 50.4 MB (25.25%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    public void swap(char[] s , int i){
+        char temp = s[i];
+        s[i] = s[s.length-1-i];
+        s[s.length-1-i] = temp;
+    }
+    
+    public void reverseString(char[] s) {
+        for(int i = 0 ; i<s.length/2 ; i++){
+            swap(s , i);
+        }
+        //return s;
+    }
+}
