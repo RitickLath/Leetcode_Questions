70cf9aff13901bd7e7f909902edc822b911b02bc
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-05 15:23:43 +0530

    Time: 5 ms (96.40%), Space: 43.9 MB (6.14%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,16 +1,24 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        if(moves.length()%2!=0){return false;}
-        int u =0;
-        int r =0;
-        
-        for(int i = 0; i<moves.length() ; i++){
-            if(moves.charAt(i) == 'R'){r++;}
-            else if(moves.charAt(i) == 'D'){u--;}
-            else if(moves.charAt(i) == 'U'){u++;}
-            else{r--;}
+        if (moves.length() % 2 != 0) {
+            return false;
         }
-        
-        return u==0 && r==0;
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        for (char move : moves.toCharArray()) {
+            if (move == 'R') {
+                horizontal++;
+            } else if (move == 'L') {
+                horizontal--;
+            } else if (move == 'U') {
+                vertical++;
+            } else if (move == 'D') {
+                vertical--;
+            }
+        }
+
+        return horizontal == 0 && vertical == 0;
     }
 }
