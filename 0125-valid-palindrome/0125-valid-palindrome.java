9cfc908f90133252e87e2ac2f3e291a504cc2d4b
Ritick Lath <114455908+RitickLath@users.noreply.github.com>
2023-06-08 02:53:11 +0530

    Time: 9 ms (49.93%), Space: 44.9 MB (7.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,25 @@
-class Solution {
+public class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
-        
+
+        Character c[] = new Character[s.length()];
         StringBuilder str = new StringBuilder();
-        
-        for(int i = 0 ; i<s.length() ; i++){
-            char c = s.charAt(i);
-            if (Character.isLetterOrDigit(c)) {
-                str.append(c);
+
+        for (int i = 0; i < s.length(); i++) {
+            c[i] = s.charAt(i);
+            if (Character.isLetterOrDigit(c[i])) {
+                str.append(c[i]);
             }
         }
-        
-        
-        
+
         s = str.toString();
-        
-        for(int i = 0 ; i<s.length()/2 ; i++){
-            if(s.charAt(i) != s.charAt(s.length()-1-i)){
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return false;
             }
         }
-        
+
         return true;
     }
 }
